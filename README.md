d3e4ad208f789a890e805c347caee2140337ce26
Devh <devendra.hazare@infinitylabs.in>
2019-06-26 11:49:12 +0530

    changes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # mac-api
 mac-api
+This script ask for mac address input if you execute on command-line
+This script also called as web api by passing token & mac address as like below link
+api-mac.py?get_token = ABC & mac = 44:38:39:ff:ef:51
--- api-mac.py
+++ api-mac.py
@@ -1,9 +1,56 @@
 import requests
 import json
-mac = input("Enter MAC ")
-#mac = '44:38:39:ff:ef:51'
+import re
+import http.client
+import cgi, cgitb
 
-response = requests.get("https://api.macaddress.io/v1?apiKey=at_6KQIokfk5IhAe8Q2gdd6Zk7kd10rS&output=json&search="+mac)
-# Print the status code of the response.
-json_str = json.loads(response.content)
-print(json_str['vendorDetails']['companyName'])
+# Create instance of FieldStorage
+form = cgi.FieldStorage()
+
+API_URL = "https://api.macaddress.io/"
+
+def auth_token():
+    #auth = request.headers.get("Authorization", None)
+    API_key = 'at_6KQIokfk5IhAe8Q2gdd6Zk7kd10rS'
+    return API_key
+
+# Ask input to user
+def ask_input():
+    mac = input("Enter MAC ")
+    if re.match("[0-9a-f]{2}([-:]?)[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$", mac.lower()):
+        result = CallApi(mac)
+        print(result)
+    else:
+        print('Invalid MAC')
+
+
+#Call API
+def CallApi(mac):
+    token_api = auth_token()
+    response = requests.get(API_URL+'v1?apiKey='+token_api+'&output=json&search='+mac)
+    # mac = '44:38:39:ff:ef:51'
+    # Print the status code of the response
+    if(response.status_code==200):
+        json_str = json.loads(response.content)
+        return_val = json_str['vendorDetails']['companyName']
+    return return_val
+
+if __name__ == "__main__":
+   #call apic from url
+   #api-mac.py?get_token = ABC & mac = 44:38:39:ff:ef:51
+    token = form.getvalue('get_token')
+    mac = form.getvalue('mac')
+    ask_input()
+    '''if token=='None':
+        ask_input()
+    else:
+        CallApi(mac)'''
+
+'''
+print("\n GET example")
+conn = http.client.HTTPSConnection("httpbin.org")
+conn.request("GET", "/get")
+response = conn.getresponse()
+data = response.read().decode('utf-8')
+print(response.status, response.reason)
+print(data)'''
